c3658b46ecbeee0740d5e384d61f0a1ffcf38fdb
mtmsujon <mtmsujon@gmail.com>
2024-08-03 17:28:10 +0600

    handle save api key and sender key

--- admin/qsms-settings-page.php
+++ admin/qsms-settings-page.php
@@ -7,6 +7,8 @@ class Settings_Page {
 
     public function setup_hooks() {
         add_action( 'admin_menu', [ $this, 'register_admin_menu' ] );
+        add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+        add_action( 'wp_ajax_save_kakao_settings', [ $this, 'save_kakao_settings' ] );
     }
 
     public function register_admin_menu() {
@@ -20,10 +22,79 @@ class Settings_Page {
         );
     }
 
+    public function enqueue_scripts() {
+        wp_enqueue_script( 'kakao-settings-js', plugin_dir_url( __FILE__ ) . 'js/qata-message-admin.js', array( 'jquery' ), null, true );
+        wp_localize_script( 'kakao-settings-js', 'kakaoSettings', array(
+            'ajax_url' => admin_url( 'admin-ajax.php' ),
+            'nonce'    => wp_create_nonce( 'kakao_settings_nonce' ),
+        )
+        );
+    }
+
     public function kakao_settings_page_html() {
         if ( !current_user_can( 'manage_options' ) ) {
             return;
         }
+        ?>
+
+        <style>
+            .input-fields {
+                margin-top: 50px;
+                width: 25%;
+            }
+
+            .api_key,
+            .sender_key {
+                display: block;
+                margin-bottom: 20px;
+                gap: 30px;
+            }
+
+            .kakao-input {
+                margin-top: 5px;
+            }
+        </style>
+
+        <div class="kakao-wrapper">
+            <h1><?php esc_html_e( 'Kakao Settings', 'qata-message' ) ?></h1>
+
+            <form id="kakao-settings-form">
+
+                <div class="input-fields">
+                    <div class="api_key">
+                        <label for="api_key"><?php esc_html_e( 'API Key:', 'qata-message' ) ?></label>
+                        <input type="text" class="widefat kakao-input" placeholder="API Key" name="api_key" id="api_key"
+                            value="<?php echo esc_attr( get_option( 'kakao_api_key' ) ); ?>">
+                    </div>
+
+                    <div class="sender_key">
+                        <label for="sender_key"><?php esc_html_e( 'Sender Key:', 'qata-message' ) ?></label>
+                        <input type="text" class="widefat kakao-input" placeholder="Sender Key" name="sender_key"
+                            id="sender_key" value="<?php echo esc_attr( get_option( 'kakao_sender_key' ) ); ?>">
+                    </div>
+                </div>
+
+                <input type="button" class="button button-primary" id="save" value="Save">
+            </form>
+        </div>
+
+        <?php
+    }
+
+    public function save_kakao_settings() {
+        check_ajax_referer( 'kakao_settings_nonce', 'nonce' );
+
+        if ( !current_user_can( 'manage_options' ) ) {
+            wp_send_json_error( 'Unauthorized user' );
+        }
+
+        $api_key    = sanitize_text_field( $_POST['api_key'] );
+        $sender_key = sanitize_text_field( $_POST['sender_key'] );
+
+        update_option( 'kakao_api_key', $api_key );
+        update_option( 'kakao_sender_key', $sender_key );
+
+        wp_send_json_success( 'Settings saved' );
     }
 }
 
